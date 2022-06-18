2d6cc7113518394d641711dea38fa8b096f2cd35
il <sh.il252255@gmail.com>
2022-06-18 15:29:09 +0300

    rename models in admin.py

--- manga_viewer/catalog/admin.py
+++ manga_viewer/catalog/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from catalog.models import *
 
 
-class TitlesAdmin(admin.ModelAdmin):
+class MangaTitlesAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'poster', 'banner')
 
 
-class BooksAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'name', 'number')
+class ChaptersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'name', 'number', 'poster')
     prepopulated_fields = {'slug': ('name',)}
 
 
-admin.site.register(Titles, TitlesAdmin)
-admin.site.register(Books, BooksAdmin)
+admin.site.register(MangaTitles, MangaTitlesAdmin)
+admin.site.register(Chapters, ChaptersAdmin)
