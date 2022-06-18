1c626ca295501714dcb0273f3fd76a057f728ad4
il <sh.il252255@gmail.com>
2022-06-18 15:22:24 +0300

    rename models in models.py

--- manga_viewer/catalog/models.py
+++ manga_viewer/catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 
-class Titles(models.Model):
+class MangaTitles(models.Model):
     origin_id = models.IntegerField(unique=True)
     title = models.TextField(unique=True)
     description = models.TextField()
@@ -19,9 +19,9 @@ class Titles(models.Model):
         return reverse('title_page', kwargs={'title_slug': self.slug})
 
 
-class Books(models.Model):
+class Chapter(models.Model):
     origin_id = models.IntegerField(unique=True)
-    title = models.ForeignKey(Titles, on_delete=models.CASCADE)
+    title = models.ForeignKey(MangaTitles, on_delete=models.CASCADE)
     number = models.FloatField()
     poster = models.ImageField(upload_to='posters/')
     name = models.TextField()
@@ -32,12 +32,15 @@ class Books(models.Model):
     def get_absolute_url(self):
         return reverse('book_page', kwargs={'book_slug': self.slug, 'title_slug': self.title.slug})
 
+    def get_number(self):
+        return str(int(self.number)) if self.number == int(self.number) else str(self.number)
+
     def __str__(self):
         return self.name
 
 
 class Pages(models.Model):
-    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     url = models.URLField(unique=True)
     number = models.IntegerField(default=0)
     size = models.IntegerField()
@@ -48,8 +51,8 @@ class Pages(models.Model):
         return self.number
 
 
-class Other_titles(models.Model):
-    title = models.ForeignKey(Titles, on_delete=models.CASCADE)
+class OtherTitles(models.Model):
+    title = models.ForeignKey(MangaTitles, on_delete=models.CASCADE)
     title_other = models.TextField()
     lang = models.CharField(max_length=4)
 
@@ -57,5 +60,9 @@ class Other_titles(models.Model):
         return self.title
 
 
+class Covers(models.Model):
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    page = models.ForeignKey(Pages, on_delete=models.CASCADE)
+    miniature = models.FileField(upload_to='covers/', null=True, blank=True)
     
 
