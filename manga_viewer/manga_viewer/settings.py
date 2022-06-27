8b879ec550ce3eab6b145c8c0814c094020e3a12
il <sh.il252255@gmail.com>
2022-06-27 13:03:32 +0300

    fix: deleted unused app folder 'viewer/'

--- manga_viewer/manga_viewer/settings.py
+++ manga_viewer/manga_viewer/settings.py
@@ -39,10 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'catalog.apps.CatalogConfig',
-    'viewer.apps.ViewerConfig'
-
 ]
 
 MIDDLEWARE = [
